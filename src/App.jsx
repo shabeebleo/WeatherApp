81c5c0a6164b2806e8279dbd2be3b078dc50104d
shabeebleo <shabeebpv0786@gmail.com>
2024-04-17 14:18:43 +0530

    issues related input and alert design

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     
     <div
-      className="h-screen flex items-center justify-center bg-cover bg-center      -z-[10]"
+      className="h-screen flex items-center justify-center bg-cover bg-center -z-[10]"
       style={{ backgroundImage: `url(${BgDrops})` }}
     >
       <>
